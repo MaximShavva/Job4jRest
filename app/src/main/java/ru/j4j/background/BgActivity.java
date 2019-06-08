8d1c96e62704efcd217c7685a1fecd9476158458
MaximShavva <saloedos@yandex.ru>
2019-06-08 17:10:27 +0300

    02 - Main (UI) Thread, runOnUiThread [#91383]
    2) Выполните указанные примеры выше.
    
    3) Добавьте ImageView в макет, добавьте загрузку картинки в фоне и обновление imageView после успешной загрузки.
       Ссылка на картинку - любая

--- app/src/main/java/ru/j4j/background/BgActivity.java
+++ app/src/main/java/ru/j4j/background/BgActivity.java
@@ -1,10 +1,18 @@
 package ru.j4j.background;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.net.URL;
 
 import ru.j4j.R;
 
@@ -28,29 +36,68 @@ public class BgActivity extends AppCompatActivity {
      */
     private volatile boolean active = false;
 
+    /**
+     * Используем этот объект для связи главного потока с соновым.
+     */
+    private Handler mainHandler = new Handler();
+
+    /**
+     * Выводит информацию о достигнутых 50%
+     */
+    private TextView reach;
+
+    /**
+     * Сюда выведем какую-либо картинку из интернета.
+     */
+    private ImageView picture;
+
+    /**
+     * УРЛ картинки.
+     */
+    private final String url = "https://kotiki.net/wp-content/uploads/2019/05" +
+            "/%D0%94%D0%9E%D0%98%D0%93%D0%A0%D0%90%D0%9B%D0%98%D0%A1%D0%AC-310x165.jpg";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bg);
+        reach = findViewById(R.id.textView);
+        picture = findViewById(R.id.imageView);
     }
 
     /**
      * Колбэк кнопки startButton.
-     * Запускает новый поток, выводит счётчик в Лог.
+     * Запускает новый поток.
+     * Разные варианты обновления IU, кроме runOnUiThread():
+     *
+     * reach.post(() -> reach.setText("Достигли 50%"));
+     *
+     * или:
+     * Handler threadHandler = new Handler(Looper.getMainLooper());
+     * threadHandler.post(() -> reach.setText("Достигли 50%"));
+     *
+     * или
+     * mainHandler.post(() -> reach.setText("Достигли 50%"));
      */
     public void startThread(View view) {
         active = true;
+        reach.setText(R.string.wait_fifty);
         new Thread(() -> {
             int count = 0;
-            while (active) {
+            while (active && count != 11) {
                 Log.d(BgActivity.TAG, "startThread: " + count);
+                if (count == 5) {
+                    runOnUiThread(() -> reach.setText("Достигли 50%"));
+                }
                 count++;
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(200);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
             }
+            Bitmap bitmap = loadImageFromNetwork(url);
+            mainHandler.post(() -> picture.setImageBitmap(bitmap));
         }).start();
     }
 
@@ -64,56 +111,17 @@ public class BgActivity extends AppCompatActivity {
     }
 
     /**
-     * Этот класс использовался в примере 01 - Thread, Runnable [#91382]
-     * Не задействован.
+     * Грузим картинку из интернета в фоне.
+     * Log: In Thread: Thread-173
      */
-    private class TestThread extends Thread {
-
-        private int times;
-
-        public TestThread(int times) {
-            this.times = times;
+    private Bitmap loadImageFromNetwork(String url) {
+        Log.d(BgActivity.TAG, "In Thread: " + Thread.currentThread().getName());
+        Bitmap bitmap = null;
+        try {
+            bitmap = BitmapFactory.decodeStream(new URL(url).openStream());
+        } catch (IOException e) {
+            bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.not_found);
         }
-
-        @Override
-        public void run() {
-            int count = 0;
-            while (count != times) {
-                Log.d(BgActivity.TAG, "startThread: " + count);
-                count++;
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    /**
-     * Этот класс использовался в примере 01 - Thread, Runnable [#91382]
-     * Не задействован.
-     */
-    private class TestRunnable implements Runnable {
-
-        private int times;
-
-        public TestRunnable(int times) {
-            this.times = times;
-        }
-
-        @Override
-        public void run() {
-            int count = 0;
-            while (count != times) {
-                Log.d(BgActivity.TAG, "startThread: " + count);
-                count++;
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+        return bitmap;
     }
 }
